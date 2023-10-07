781079d036393ba1fac08adfb2776b0a0bc762b6
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-10-07 12:12:57 +0700

    app(): change modules to pages

--- src/app/pages/movie/movie-detail/movie-detail.component.ts
+++ src/app/pages/movie/movie-detail/movie-detail.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MoviesService } from 'src/app/core/services/movies.service';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
-  styleUrls: ['./movie-detail.component.css']
 })
-export class MovieDetailComponent {
+export class MovieDetailComponent implements OnInit {
+  movie: any;
+  constructor(private service: MoviesService, private router: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    let getParamId = this.router.snapshot.paramMap.get('id');
+    console.log(getParamId);
+
+    this.getMovie(getParamId);
+  }
+
+  getMovie(id: any) {
+    this.service.getMovieDetails(id).subscribe((res) => {
+      this.movie = res;
+    });
+  }
 }
--- src/app/pages/user/user-favorite/user-favorite.component.ts
+++ src/app/pages/user/user-favorite/user-favorite.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from 'src/app/core/services/movies.service';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-user-favorite',
   templateUrl: './user-favorite.component.html',
-  styleUrls: ['./user-favorite.component.css']
 })
-export class UserFavoriteComponent {
+export class UserFavoriteComponent implements OnInit {
+  faStar = faStar;
+  favouriteMovie: any = [];
+  constructor(private service: MoviesService) {}
 
+  ngOnInit(): void {
+    this.getFavouriteMovie();
+  }
+
+  getFavouriteMovie() {
+    this.service.getFavourites().subscribe((res) => {
+      this.favouriteMovie = res.results;
+      console.log(res.results);
+    });
+  }
+
+  removeFavourite(id: number) {
+    let data = {
+      media_type: 'movie',
+      media_id: id,
+      favorite: false,
+    };
+    this.service.handleFavourite(data).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
