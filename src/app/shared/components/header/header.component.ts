d207425a4ca8b2045d3c6d50ac1a14b04cb862ff
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-10-08 11:52:18 +0700

    app(): add hamburger menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,7 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  faBars = faBars;
+  faX = faX;
+
+  isOpen: boolean = false;
+  constructor() {}
+
+  ngOnInit() {
+    this.menuClick(this.isOpen);
+  }
+
+  menuClick(b: boolean) {
+    this.isOpen = b;
+    if (this.isOpen == true) {
+      console.log('isOpen');
+    } else if (this.isOpen == false) {
+      console.log('isNotOpen');
+    }
+  }
+}
