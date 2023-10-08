6acae504fa185a0fb88622187b88bbf8d935f954
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-10-09 00:45:02 +0700

    app(): add PWA so app can run in online mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { AppComponent } from './app.component';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -20,6 +20,7 @@ import {
 import { MovieDetailsComponent } from './pages/movie/movie-details/movie-details.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ListComponent } from './shared/components/list/list.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -41,6 +42,12 @@ import { ListComponent } from './shared/components/list/list.component';
     FontAwesomeModule,
     NgbModule,
     InfiniteScrollModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
