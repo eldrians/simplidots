4ab5308112b5ae94a6b0ea204dc3fa54ab446150
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-10-09 16:15:24 +0700

    app(): add alert for add/remove favorite

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -8,21 +8,30 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [CommonModule, FontAwesomeModule],
   template: `
     <button
-      class="p-1 border border-primaryApp bg-white hover:bg-primaryApp/60 rounded text-xs md:text-sm"
+      class="p-1 border border-primaryApp group rounded text-xs md:text-sm"
     >
-      <p class="text-primaryApp/80 " *ngIf="varType() == true">
+      <p
+        class="text-primaryApp/80 group-hover:text-yellow-500 "
+        *ngIf="varType() == true"
+      >
         {{ title }}
       </p>
       <fa-icon
-        *ngIf="varType() == false"
+        *ngIf="varType() == false && toAdd == true"
         [icon]="title"
-        class="text-primaryApp/80"
+        class="text-primaryApp/80 hover:text-yellow-500"
+      ></fa-icon>
+      <fa-icon
+        *ngIf="varType() == false && toAdd == false"
+        [icon]="title"
+        class="text-yellow-500"
       ></fa-icon>
     </button>
   `,
 })
 export class ButtonComponent {
   @Input() title: any;
+  @Input() toAdd?: boolean;
 
   varType(): boolean {
     return typeof this.title === 'string';
--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -9,6 +9,8 @@ import { UserService } from '../../../../app/core/services';
 import { ButtonComponent } from '../button/button.component';
 
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import Swal from 'sweetalert2';
 
 type TFavoriteHandler = {
   media_type: string;
@@ -21,6 +23,7 @@ type TFavoriteHandler = {
   standalone: true,
   imports: [
     RouterModule,
+    SweetAlert2Module,
     NgbModule,
     FontAwesomeModule,
     DatePipe,
@@ -36,7 +39,7 @@ export class CardComponent {
 
   constructor(private userServices: UserService) {}
 
-  setFavorite(id: number) {
+  setFavorite(id: number, title: string) {
     if (this.toAdd == true) {
       this.favoriteHandler = {
         media_type: 'movie',
@@ -51,8 +54,21 @@ export class CardComponent {
       };
     }
     this.userServices.favoriteHandler(this.favoriteHandler).subscribe((res) => {
-      
-      // return alert
+      if (this.toAdd) {
+        Swal.fire(
+          'Difavoritkan',
+          `Film ${title} ditambahkan ke favorite`,
+          'success'
+        );
+      } else {
+        Swal.fire(
+          'Dihapus',
+          `Film ${title} dihapus dari favorite`,
+          'error'
+        ).then(() => {
+          location.reload();
+        });
+      }
     });
   }
 
