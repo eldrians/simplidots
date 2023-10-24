90f51e0fc1ed02e3bbf70776b230ad8736158089
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-10-25 02:45:54 +0700

    app(): delete app.html and combine in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,22 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html',
+  template: `
+    <div class="w-full flex flex-col lg:flex-row">
+      <div class="hidden lg:block lg:w-1/5">
+        <app-sidebar class="hidden lg:block lg:w-1/5 lg:fixed"></app-sidebar>
+      </div>
+      <div class="w-full lg:w-4/5">
+        <div class="w-full lg:w-4/5 h-[50px]">
+          <app-header class="w-full lg:w-4/5 h-[50px] fixed z-50"></app-header>
+        </div>
+        <router-outlet class="z-10"></router-outlet>
+      </div>
+      <div class="w-full block lg:hidden">
+        <app-footer></app-footer>
+      </div>
+    </div>
+  `,
   imports: [SidebarComponent, HeaderComponent, FooterComponent, RouterModule],
 })
 export class AppComponent {
